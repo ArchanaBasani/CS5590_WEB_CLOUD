a8e1e3112ddff21e8eb4579eba80e2bddaded75f
ArchanaBasani <archanareddychaitu@gmail.com>
2018-06-29 21:42:59 -0500

    commit

--- Lab Assignments/Assignment 2/source/TwitterFriends/app.js
+++ Lab Assignments/Assignment 2/source/TwitterFriends/app.js
@@ -1,60 +1,82 @@
-var angular;
 var app = angular.module("TwitterAPP", []);
-
-app.controller = function (s, f) {
-    
-};
 app.controller("serviceCtrl", function($scope, $http) {
     "use strict";
     $scope.search = function() {
-        var url = "http://127.0.0.1:8000/?screen_name=" + $scope.screen_name;
-        var svg = d3.select("svg");
-        var margin = 200;
-        var width = svg.attr("width") - margin;
-        var height = svg.attr("height") - margin;
-        var xAxis = d3.scaleBand().range([0, width]).padding(0.2);
-        var yAxis = d3.scaleLinear().range([height, 0]);
-        var g = svg.append("g").attr("transform", "translate(" + 100 + "," + 100 + ")");
+        var root;
+//        var url = "http://127.0.0.1:8000/?screen_name=" + $scope.screen_name;
+        console.log($scope.screen_name)
+        $http.get('http://127.0.0.1:8000/'+$scope.screen_name).success(function (data) {
+            alert("Inn")
+            d3.select('svg').remove();
 
-        d3.json(url, function(error, data) {
-            if (error) {
-                throw error;
-            }
-            // Name for each user
-            var names = [];
-            // Friends count for each user
-            var count = [];
-            // Final data
-            var parsed_data = [];
-            // Parse the JSON data
-            for (var counter in data.users) {
-                names.push(data.users[counter].screen_name);
-                count.push(data.users[counter].friends_count);
-            }
-            // Calculate the sum of array count
-            var sum = count.reduce(function(l, r) { return l + r}, 0);
-            // Match each name with frequency (frequency of freiend count) - Only show 5 records max
-            var record = names.length > 5? 5 : names.length;
-            for (var i = 0; i < record; i++) {
-                parsed_data.push({
-                    name: names[i],
-                    frequency: parseInt(count[i] / sum * 100)
+            // ************** Generate the tree diagram	 *****************
+            var margin = {top: 20, right: 120, bottom: 20, left: 120},
+                width = 960 - margin.right - margin.left,
+                height = 500 - margin.top - margin.bottom;
+            var i = 0;
+            var tree = d3.layout.tree()
+                .size([height, width]);
+            var diagonal = d3.svg.diagonal()
+                .projection(function(d) { return [d.y, d.x]; });
+            var svg = d3.select("body").append("svg")
+                .attr("width", width + margin.right + margin.left)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            // load the external data
+            d3.json("output.json", function(error, treeData) {
+                root = treeData[0]
+                update(root);
+            });
+            function update(source) {
+                // Compute the new tree layout.
+                var nodes = tree.nodes(root),
+                    links = tree.links(nodes);
+                // Normalize for fixed-depth.
+                nodes.forEach(function (d) {
+                    d.y = d.depth * 180;
                 });
+                // Declare the nodes…
+                var node = svg.selectAll("g.node")
+                    .data(nodes, function (d) {
+                        return d.name || (d.name = ++i);
+                    });
+                // Enter the nodes.
+                var nodeEnter = node.enter().append("g")
+                    .attr("class", function (d) {
+                        if (d.parent == "null") {
+                            return "node parent"
+                        } else
+                            return "node child"
+                    })
+                    .attr("transform", function (d) {
+                        return "translate(" + d.y + "," + d.x + ")";
+                    });
+                nodeEnter.append("circle")
+                    .attr("r", 10)
+                    .style("fill", "#fff")
+                nodeEnter.append("text")
+                    .attr("x", function (d) {
+                        return 13
+                    })
+                    .attr("dy", ".35em")
+                    .attr("text-anchor", function (d) {
+                        return d.parent || d._children ? "start" : "end";
+                    })
+                    .text(function (d) {
+                        return d.name;
+                    })
+                    .style("fill-opacity", 1);
+                // Declare the links…
+                var link = svg.selectAll("path.link")
+                    .data(links, function (d) {
+                        return d.target.id;
+                    });
+                // Enter the links.
+                link.enter().insert("path", "g")
+                    .attr("class", "link")
+                    .attr("d", diagonal);
             }
-            // X axis
-            xAxis.domain(parsed_data.map(function(d) {return d.name;}));
-            // Y axis
-            yAxis.domain([0, d3.max(parsed_data, function(d) {return d.frequency;})]);
-            // D3
-            g.append("g").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(xAxis));
-            g.append("g").call(d3.axisLeft(yAxis).tickFormat(function(d) {return d + "%";}).ticks(15));
-            g.selectAll(".bar").data(parsed_data).enter().append("rect")
-                .attr("class", "bar")
-                .attr("x", function(d) {return xAxis(d.name);})
-                .attr("y", function(d) {return yAxis(d.frequency);})
-                .attr("width", xAxis.bandwidth())
-                .attr("height", function(d) {return height - yAxis(d.frequency);});
-            g.append("text").attr("x", width / 2 ).attr("y", -50).style("text-anchor", "middle").text("Screen Name VS. Friends Count Frequency");
         });
     };
 });
--- Lab Assignments/Assignment 2/source/TwitterFriends/server.js
+++ Lab Assignments/Assignment 2/source/TwitterFriends/server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var cors = require("cors");
 var bodyParser = require("body-parser");
 var Twitter = require("twitter");
+var fs = require('fs');
+var request = require('request')
 
 var app = express();
 app.use(cors());
@@ -12,6 +14,14 @@ app.use(bodyParser.urlencoded({
 app.set("port", process.env.PORT || 8000);
 app.set("host", process.env.HOST || "0.0.0.0");
 
+// For CORS Issue
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
+
 var client = new Twitter({
     consumer_key: "fdxhp1vvyrjZgAiULWRqy775F",
     consumer_secret: "cI0nr2V9DP3Vdijm7oqsITFVYVGpZVLd3JpRXRt6VoniSJcB1Q",
@@ -19,19 +29,25 @@ var client = new Twitter({
     access_token_secret: "vzcknjYTLifThNJ6RUwStID41x75JBdSlA2rEpmamwPSq"
 });
 
-app.get("/", function(req, res) {
-    var name = req.query.screen_name;
-
-    client.get("friends/list", {
-        screen_name: name
-    }, function(error, tweets, response) {
-        if (!error) {
-            res.json(tweets);
-        } else {
-            res.status(500).json({
-                error: error
-            });
+app.get('/:screen_name',function(req,res){
+    var name = req.params.screen_name;
+    var url = 'https://api.twitter.com/1.1/friends/list.json?cursor=-1&skip_status=true&include_user_entities=false&screen_name='+name;
+    request({ url: url,
+        method:'GET',
+        headers: {
+            "Authorization": "Bearer AAAAAAAAAAAAAAAAAAAAANNn6wAAAAAAVkbD41b%2B3z%2BfHRhclqcynxAqLz8%3DyNDucgxspilLHOhWLpoJ2I0KVqdfxetjEJVftu7F5KgWJ2jXU3"
         }
+    }, function(err, resp, data) {
+        var data1 = JSON.parse(data);
+        // Stringfying the Data
+        var data2 = JSON.stringify([data1], null, 2);
+        var obj = JSON.parse(data2)[0];
+        console.log(obj);
+        obj.children = obj.users;
+            delete obj.users;
+            data2 = JSON.stringify([obj], null, 2);
+            fs.writeFileSync('output.json',data2);
+            res.send("Success");
     });
 });
 
